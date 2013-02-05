4a4f9a6700fe483fa7dffdf6bbc2d7533ffd4787
frobiac <frobiac@gmail.com>
2013-02-05 20:28:52 +0100

    IGNORE TODO: matrix definition and pin config should be modular

--- src/matrix.h
+++ src/matrix.h
@@ -22,76 +22,70 @@
 #include <avr/io.h>
 
 
-uint8_t read_col(void);
 void unselect_rows(void);
 void activate(uint8_t row);
 
 // this must be called once before matrix_scan.
-uint8_t read_col(void)
-{
-#if defined(__AVR_ATmega32U4__)
-    uint8_t res = PINF;
-    // flipped teensy 2.0: F 01  4567
-    return (( res & 0b11) | ((res & 0b11110000)>>2 ));
-#elif defined(__AVR_AT90USB1286__)             // Teensy++ 2.0
-    // teensy++ 2.0 n // PINF & 0x11
-    return (PINF & ~((1<<6) | (1<<7)));
+static inline uint16_t read_col(void)
+{   // Teensy 2.0                  // Teensy++ 2.0
+#if defined(__AVR_ATmega32U4__) || defined(__AVR_AT90USB1286__)
+    // 12 cols ordered left to right: PIND:0123457 and PINF:34567
+    uint16_t resLow=PIND & 0b10111111;
+    uint16_t resHigh=PINF;
+    uint16_t resHigh1=(resHigh & 0b11110000)<<4;
+    uint16_t resHigh0=(resHigh & 0b00001000)<<3;
+//    uint16_t resHigh=(PINF & 0b11110000)<<4;
+    uint16_t result = (resHigh1 | resHigh0 | resLow);
+    // lets debug
+//    printf ("\n E: ");
+//    for(int i=(sizeof(result) * 8) - 1; i >= 0;i--)
+//    {
+////         printf("%d",(resHigh0 & (0x01 << 6)) > 0);
+//     printf("%d",(result & (0x01 << i)) > 0);
+//    }
+//    printf (":E\n");
+    return result;
+#else
+#error "weder Teensy 2 noch 2++ eingestellt"
+    return 0;
 #endif
 }
 
 void unselect_rows(void)
-{
-#if defined(__AVR_ATmega32U4__)
-    DDRD  &= 0b00001011;
-    PORTD &= 0b00001011;
-    DDRB  &= 0b10001111;
-    PORTB &= 0b10001111;
-#elif defined(__AVR_AT90USB1286__)             // Teensy++ 2.0
-#error "PINCONFIG for unselect_rows NOT defined for __AVR_AT90USB1286__ setup"
+{   // Teensy 2.0                  // Teensy++ 2.0
+#if defined(__AVR_ATmega32U4__) || defined(__AVR_AT90USB1286__)
+    // B:76543210 in use
+    // set all row pins as inputs
+    DDRB  &= 0b00000000;
+    // drive all row pins low
+    PORTB &= 0b00000000;
 #endif
 }
 
 void activate(uint8_t row)
-{
-#if defined(__AVR_ATmega32U4__)
+{   // Teensy 2.0                  // Teensy++ 2.0
+#if defined(__AVR_ATmega32U4__) || defined(__AVR_AT90USB1286__)
     unselect_rows();
-    // swap upper and lower ports to have left half first in matrix
-    (row<4) ? (row+=4) : (row-=4);
-    // B6 B5 B4 D7
-    // D6 D4 D2 D5
-    switch(row) {
-        case 0: DDRB |= (1<<6); break;
-        case 1: DDRB |= (1<<5); break;
-        case 2: DDRB |= (1<<4); break;
-        case 3: DDRD |= (1<<7); break;
-        case 4: DDRD |= (1<<6); break;
-        case 5: DDRD |= (1<<4); break;
-        case 6: DDRD |= (1<<2); break;
-        case 7: DDRD |= (1<<5); break;
+    // B:76543210 in use
+    // set current row pin as output
+    if (row<8) {
+        DDRB |= (1<<row);
     }
     return;
-
-#elif defined(__AVR_AT90USB1286__)             // Teensy++ 2.0
-#error "PINCONFIG for activate(row) NOT defined for __AVR_AT90USB1286__ setup"
 #endif
 }
 
-/// @todo Why static inline only here ?
 static inline void init_cols(void)
-{
-#if defined(__AVR_ATmega32U4__)
-    // teensy 2.0: 2&3 unused, F 01  4567
-    /* Columns are inputs */
-    DDRF  &= ((1<<2) | (1<<3)); // 192 or upper 2 bits
+{   // Teensy 2.0                  // Teensy++ 2.0
+#if defined(__AVR_ATmega32U4__) || defined(__AVR_AT90USB1286__)
+    // 12 cols ordered left to right: PIND:0123457 and PINF:34567
+    // set all column pins as inputs
+    DDRD  &= 0b00000000;
+    DDRF  &= 0b00000111;
     /* Enable pull-up resistors on inputs */
-    PORTF |= (0b11110011);
-
-#elif defined(__AVR_AT90USB1286__)
-    // teensy++ 2.0 : 6&7 unused
-    DDRF &= ((1<<6) | (1<<7));
-    PORTF |= (0b00111111);
+    PORTD |= (0b11111111);
+    PORTF |= (0b11111000);
     return;
 #endif
-
 }
 #endif
